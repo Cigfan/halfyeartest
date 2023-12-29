23261ad20825034a5ed720bcddd13138041839db
Cigfan <anisenyst@yandex.ru>
2023-12-29 19:19:56 +0300

    3

--- test.py
+++ test.py
@@ -1,17 +1,16 @@
-"""col = ['Астрахань','Три','4','Вера','Да']
+col = ['Астрахань','Три','4','Вера','Да']
 #print (col)
 res = []
 for i in col:
-    if len(col[i]) <= 3:
-        res = res + col[i]
+    if len(i) <= 3:
+        res.append(i)
 print(res)
-      """
 
-a = len(col[0])
+"""a = len(col[0])
 b = len(col[1])
 c = len(col[2])
 d = len(col[3])
 e = len(col[4])
 
 print(a, b,c ,d, e)
-print(type(a), type(b), type(c), type(d), type(e))
+print(type(a), type(b), type(c), type(d), type(e))"""
