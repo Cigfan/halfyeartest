cd4b9b802ba4b0fd3fcb542d2458af652a4ec5bc
Cigfan <anisenyst@yandex.ru>
2023-12-29 21:22:29 +0300

    fin

--- test.py
+++ test.py
@@ -1,17 +1,14 @@
 col = str(input("Введите данные через пробел: ")).split()
-#col = ['Астрахань','Три','4','Вера','Да']
-#print (col)
+
+#Вариант 1
 res = []
 for i in col:
     if len(i) <= 3:
         res.append(i)
 print(res)
 
-"""a = len(col[0])
-b = len(col[1])
-c = len(col[2])
-d = len(col[3])
-e = len(col[4])
-
-print(a, b,c ,d, e)
-print(type(a), type(b), type(c), type(d), type(e))"""
+#Вариант 2
+for i in col:
+    if len(i) > 3:
+        col.remove(i)
+print(col)
