f515713993fcef8e19d752d0aed62acd3bf8c8b9
Cigfan <anisenyst@yandex.ru>
2023-12-29 19:17:15 +0300

    2

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
-col = ['Астрахань','Три','4','Вера','Да']
-print (col)
+"""col = ['Астрахань','Три','4','Вера','Да']
+#print (col)
+res = []
+for i in col:
+    if len(col[i]) <= 3:
+        res = res + col[i]
+print(res)
+      """
+
+a = len(col[0])
+b = len(col[1])
+c = len(col[2])
+d = len(col[3])
+e = len(col[4])
+
+print(a, b,c ,d, e)
+print(type(a), type(b), type(c), type(d), type(e))
